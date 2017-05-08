e6a419dc3e716486aef5f6f3191d6c8ef41efbb2
José Sánchez <joseynene@hotmail.com>
2017-05-09 00:36:25 +0200

    Modify JSONProcessing

--- ShopsApp/ShopsApp/JSONProcessing.swift
+++ ShopsApp/ShopsApp/JSONProcessing.swift
@@ -11,8 +11,7 @@ import Foundation
 import CoreData
 
 //MARK: - Constants
-let urlFileJSON : String = "https://t.co/K9ziV0z3SJ"
-let nameFileJSON = "books_readable.json"
+let urlFileJSON : String = "http://madrid-shops.com/json_new/getShops.php"
 let shopJSONDataKey = "ShopJSONDataKey"
 
 //MARK: - Aliases
@@ -39,66 +38,95 @@ func getMyDocumentsURL() -> URL {
 
 //MARK: - Download JSON
 // Función que descarga el fichero JSON (si aplica) y lo guarda en Local en el path de Documents
-func downloadAndSaveJSONFile() throws -> Data {
+func downloadAndSaveJSONFile(success: @escaping (Data) -> Void, failure: @escaping (Error) -> Void) {
+
     // Se comprueba si la aplicación se ha ejecutado anteriormente en algún momento mediante la carga del fichero desde NSUserDefaults
     guard let shopJSONData: Data = UserDefaults.standard.data(forKey: shopJSONDataKey) else {
         // No se ha podido obtener el fichero JSON, por lo que se procede a su descarga
         // Se recupera la URL del JSON
-        guard let jsonUrl = URL(string: urlFileJSON) else {
-            throw ShopError.wrongURLFormatForJSONResource
+        guard let apiURL = URL(string: urlFileJSON) else {
+            failure(ShopError.wrongURLFormatForJSONResource)
+            return
         }
         
-        // Se recupera el JSON referenciado por la URL
-        guard let jsonDownloadedData = try? Data(contentsOf: jsonUrl) else {
-            throw ShopError.dataCollectionPointedByURLNotReachable
-        }
-        
-        // Se almacena el fichero JSON en NSUserDefaults
-        UserDefaults.standard.set(jsonDownloadedData, forKey: shopJSONDataKey)
-        
-        // Se retorna el fichero JSON
-        return jsonDownloadedData
+        URLSession.shared.dataTask(with: apiURL) { (data, response, error) in
+            if(error != nil){
+                print("error")
+            }else{
+                do{
+                    guard let jsonDownloadedData = data else {
+                        throw ShopError.dataCollectionPointedByURLNotReachable
+                    }
+                    
+                    // Se almacena el fichero JSON en NSUserDefaults
+                    UserDefaults.standard.set(jsonDownloadedData, forKey: shopJSONDataKey)
+                    
+                    // Se retorna el fichero JSON
+                    success(jsonDownloadedData)
+                }catch let error as NSError{
+                    print(error)
+                }
+            }
+        }.resume()
+        return
     }
     
     // Se ha podido obtener el fichero desde NSUserDefaults
-    return shopJSONData
+    success(shopJSONData)
 }
 
 //MARK: - Loading JSON from local file (SandBox)
 // Función que carga un fichero en local y retorna un array de JSON
 func loadJSONFromSandBox(context: NSManagedObjectContext) throws -> [Shop] {
-    do {
+    
+    var array2 : [Shop] = []
+    //do {
         // Se carga el fichero desde NSUserDefauls
-        let jsonData = try downloadAndSaveJSONFile()
+        //let jsonData = try downloadAndSaveJSONFile()
+
         
-        // Se descarga la información del fichero JSON y se parsea a un array de JSON
-        guard let maybeArray = try? JSONSerialization.jsonObject(with: jsonData, options: JSONSerialization.ReadingOptions.mutableLeaves) as? [[String: String]],
-            let array = maybeArray else {
-                throw ShopError.jsonParsingError
-        }
-        
-        // Se ha descargado correctamente el fichero a local y se ha transformado el diccionario JSON a Shops
-        return array.flatMap({ (dict: [String : String]) -> Shop? in
-            guard let id: String = dict["id"],
-                let name: String = dict["name"],
-                let address: String = dict["address"],
-                let telephone: String = dict["telephone"],
-                let email: String = dict["email"],
-                let gps_lat: String = dict["gps_lat"],
-                let gps_lon: String = dict["gps_lon"],
-                let img: String = dict["img"],
-                let logo_img: String = dict["logo_img"],
-                let description_es: String = dict["description_es"],
-                let description_en: String = dict["description_en"] else {
-                    return nil
-            }
-            
-            // Se ha recuperado todo correctamente
-            let shop = Shop(id: id, name: name, address: address, telephone: telephone, email: email, gps_lat: gps_lat, gps_lon: gps_lon, img: img, logo_img: logo_img, description_es: description_es, description_en: description_en, context: context)
-            
-            return shop
-        })
-    } catch {
-        throw ShopError.dataCollectionPointedByURLNotReachable
-    }
+        // Se ejecuta la petición
+        downloadAndSaveJSONFile(
+                         // Se imprime el perfil en caso de que todo haya ido bien
+            success: {
+                // Se descarga la información del fichero JSON y se parsea a un array de JSON
+                do {
+                    guard let maybeArray = try? JSONSerialization.jsonObject(with: $0, options: JSONSerialization.ReadingOptions.mutableLeaves) as? [[String: String]],
+                        let array = maybeArray else {
+                       throw ShopError.jsonParsingError
+                    }
+
+                
+                    // Se ha descargado correctamente el fichero a local y se ha transformado el diccionario JSON a Shops
+                    array2 = array.flatMap({ (dict: [String : String]) -> Shop? in
+                        guard let id: String = dict["id"],
+                            let name: String = dict["name"],
+                            let address: String = dict["address"],
+                            let telephone: String = dict["telephone"],
+                            let email: String = dict["email"],
+                            let gps_lat: String = dict["gps_lat"],
+                            let gps_lon: String = dict["gps_lon"],
+                            let img: String = dict["img"],
+                            let logo_img: String = dict["logo_img"],
+                            let description_es: String = dict["description_es"],
+                            let description_en: String = dict["description_en"] else {
+                                return nil
+                        }
+                    
+                        // Se ha recuperado todo correctamente
+                        let shop = Shop(id: id, name: name, address: address, telephone: telephone, email: email, gps_lat: gps_lat, gps_lon: gps_lon, img: img, logo_img: logo_img, description_es: description_es, description_en: description_en, context: context)
+                    
+                        return shop
+                    })
+                }catch let error as NSError{
+                    print(error)
+                }
+                //return array2
+        },
+        // Se imprime el error en caso en que algo haya fallado
+        failure: { print($0) })
+    //} catch {
+      //  throw ShopError.dataCollectionPointedByURLNotReachable
+   // }
+    return array2
 }
