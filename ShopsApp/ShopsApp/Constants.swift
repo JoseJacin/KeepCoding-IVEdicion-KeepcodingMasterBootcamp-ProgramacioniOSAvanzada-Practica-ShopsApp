88dfe72de113fcd19bb92134a1cf3af6d79380fa
José Sánchez <joseynene@hotmail.com>
2017-05-08 19:57:04 +0200

    Refactored CoreDataStack and modify AppDelegate. Add Constants.

---
+++ ShopsApp/ShopsApp/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  ShopsApp
+//
+//  Created by Jose Sanchez Rodriguez on 8/5/17.
+//  Copyright © 2017 Jose Sanchez Rodriguez. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    //MARK: - Database
+    static let dbName = "ShopsApp"
+}
