d15dffdf7708efeeddc67508507a17cea0201b40
José Sánchez <joseynene@hotmail.com>
2017-05-09 07:34:36 +0200

    Modify ShopsViewController+ShopTableViewCell (left align Name)

---
+++ ShopsApp/ShopsApp/ShopsViewController+ShopsMapView.swift
@@ -0,0 +1,9 @@
+//
+//  ShopsViewController+ShopsMapView.swift
+//  ShopsApp
+//
+//  Created by Jose Sanchez Rodriguez on 9/5/17.
+//  Copyright © 2017 Jose Sanchez Rodriguez. All rights reserved.
+//
+
+import Foundation
